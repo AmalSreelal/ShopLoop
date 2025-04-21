6e8759b351d9c6e988bcc4e54383d950fb707020
AMAL SREELAL <amalspillaihalo@gmail.com>
2025-04-22 00:12:24 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-    mode: 'sandbox',
-    client_id: 'AZJhnxpBV6xipSCPteXxMqFoeGaRIvGtjnQoKTnZbV21emGP2LPAoAsgm6OScHtXDeclzDG9Kg-5m1YV', // your client_id
-    client_secret : 'EEGFbLpp6twizeWnAZhzOftMTNhRDeb0qZlkF6zUnJQeUdcX4ybD7zcTypzABztFDv1m6azx1YwBDkWP', // your client_secret
-})
+  mode: "sandbox",
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+});
 
 module.exports = paypal;
